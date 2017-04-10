5d4a691a82cd385c33d15ac11aaf373aae84f30a
Bao Nguyen <baonguyena1@gmail.com>
2017-04-10 12:36:39 +0700

    Initial Commit

---
+++ NetworkStatusMonitor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NetworkStatusMonitor
+//
+//  Created by Bao Nguyen on 4/10/17.
+//  Copyright © 2017 Bao Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
